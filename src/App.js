52f7c5524c9b6b534cf46b87c373f67f4790627d
Giuseppe Sansone <giuseppesansone144@gmail.com>
2024-09-24 17:27:43 +0200

    done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,17 @@
 import React from 'react';
 import MorraCinese from './components/MorraCinese';
-import './App.css';
-import backgroundImage from './components/_2ae73e80-31d5-44c5-a55b-adce8d0914e4.jpg'; // Assicurati che il percorso sia corretto
+import backgroundImage from './components/_2ae73e80-31d5-44c5-a55b-adce8d0914e4.jpg';
 
 function App() {
+  const appStyle = {
+    backgroundImage: `url(${backgroundImage})`,
+    backgroundSize: 'cover', // Copre l'intera area
+    backgroundPosition: 'center', // Centro dell'immagine
+    height: '100vh', // Altezza intera della finestra
+  };
+
   return (
-    <div 
-      className="App" 
-      style={{ 
-        backgroundImage: `url(${backgroundImage})`, 
-        backgroundSize: 'cover', 
-        backgroundPosition: 'center', 
-        height: '100vh', 
-        color: 'white' // Modifica il colore del testo per una migliore leggibilità
-      }}
-    >
+    <div className="App" style={appStyle}>
       <MorraCinese />
     </div>
   );
